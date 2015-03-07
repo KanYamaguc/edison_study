a146daace4e3be279b66856b1c4a3883ea249928
YamaKan <yamakan.67@gmail.com>
2015-03-07 16:05:25 +0000

    add range

--- dOut.js
+++ dOut.js
@@ -1,8 +1,8 @@
 var mraa = require("mraa"); //require mraa
-var value = 0.0;
-var ZERO_SIG = 50/2000;
-var HARF_SIG = 1050/2000;
-var i = 0;
+
+var ZERO_SIG = 90/2000; // 0%
+var HARF_SIG = 120/2000; // 50%
+var MAX_SIG = 180/2000; // 100%
 
 //Initialize PWM on Digital Pin #3 (D3) and enable the pwm pin
 var pwm3 = new mraa.Pwm(3);
@@ -12,12 +12,16 @@ pwm3.enable(true);
 pwm3.period_us(20 * 1000); //50hz
 pwm3.pulsewidth_us(2000);  //palus 2ms 
 
+var i = 0;
 setInterval(function () {
-	if (i < 1500 ){
+	if (i < 2500 ){
 		pwm3.write(ZERO_SIG); // 0.0 % 
 	 	i++;
-	} else {
+	} else if( i < 4000){
 		pwm3.write(HARF_SIG); // 50 %
+	 	i++;
+	} else {
+		pwm3.write(ZERO_SIG);
 	}
-	console.log(pwm3.read());//read current value that is set before.
-}, 30); // sleep sycle 0.2 s
+	console.log(pwm3.read(),i);
+}, 2); // sleep sycle 0.2 s
